9e28a351709387f0efc1a852a8a59c3165b3918c
李超群 <121708983@qq.com>
2021-01-25 11:33:42 +0800

    修改lua

--- socketProtocol.lua
+++ socketProtocol.lua
@@ -1,23 +1,23 @@
-local my_proto = Proto("QIESocket", "企鹅直播的 socket 协议解析", "tcp sample protocol")
+local my_protocol = Proto("QIESocket", "企鹅直播的 socket 协议解析", "企鹅直播socket协议")
 
 --定义协议字段内容
-local dataLen = ProtoField.uint32("dataLen", "包的长度", base.DEC);
-local headLen = ProtoField.uint16("headLen", "header 长度", base.DEC);
-local version = ProtoField.uint16("version", "版本号", base.DEC);
-local operation = ProtoField.uint32("operation", "操作码", base.DEC);
-local seqNO = ProtoField.uint32("seqNO", "确认号", base.DEC);
-local content = ProtoField.string("content", "内容", base.STRING);
+local dataLen = ProtoField.uint32("qiesocket.dataLen", "包的长度", base.DEC);
+local headLen = ProtoField.uint16("qiesocket.headLen", "header 长度", base.DEC);
+local version = ProtoField.uint16("qiesocket.version", "版本号", base.DEC);
+local operation = ProtoField.uint32("qiesocket.op", "操作码", base.DEC);
+local seqNO = ProtoField.uint32("qiesocket.seq", "序号", base.DEC);
+local content = ProtoField.string("qiesocket.content", "内容", base.STRING);
 
-my_proto.fields = {dataLen, headLen, version, operation,  seqNO, content}
+my_protocol.fields = {dataLen, headLen, version, operation,  seqNO, content}
 --协议分析器
-function my_proto.dissector(buffer, pinfo, tree)
+function my_protocol.dissector(buffer, pinfo, tree)
 pinfo.cols.protocol:set("QIESocket")
     local len = buffer:len()
 	local offset = 0
 
 	while(offset ~= len)
 	do
-	    local myProtoTree = tree:add(my_proto, buffer(0, len), "tcp sample protocol")
+	    local myProtoTree = tree:add(my_protocol, buffer(0, len), "企鹅直播socket协议")
 
 	    myProtoTree:add(dataLen, buffer(offset, 4))
 	    dataLenCurrent = buffer(offset, 4):uint()
@@ -44,8 +44,11 @@ pinfo.cols.protocol:set("QIESocket")
 end
 
 local tcp_table = DissectorTable.get("tcp.port")
-portArray = {3001, 3101, 3201, 31010}
-for index = 1, #portArray do
-	tcp_table:add(portArray[index], my_proto)
+for index = 6410, 6420, 1 do
+	tcp_table:add(index, my_protocol)
+tcp_table:add(3101, my_protocol)
+tcp_table:add(3201, my_protocol)
+tcp_table:add(31010, my_protocol)
+tcp_table:add(3001, my_protocol)
 end
 
